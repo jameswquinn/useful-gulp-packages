e92f4432db1967af3d08bd39e4851685dd0ad09d
jameswquinn <jameswquinn@users.noreply.github.com>
2017-12-08 23:47:18 +0000

    update

--- gulpInProgress/gulp/tasks/sass&uncss.js
+++ gulpInProgress/gulp/tasks/sass&uncss.js
@@ -3,6 +3,7 @@
 var gulp = require("gulp"),
     uncss = require("gulp-uncss"),
     sass = require("gulp-sass"),
+    sourcemaps = require('gulp-sourcemaps'),
     rev = require('gulp-rev'),
     minifyCSS = require("gulp-minify-css"),
     rename = require("gulp-rename"),
@@ -14,10 +15,12 @@ gulp.task("build:sass", function () {
         .pipe(uncss({
             html: glob.sync("dist/**/*.html")
         }))
+        .pipe(sourcemaps.init({largeFile: true}))
         .pipe(minifyCSS())
         .pipe(rename({
             suffix: ".min"
         }))
         .pipe(rev())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest("dist/css"));
 });
